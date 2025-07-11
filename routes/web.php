a6a78ee9de62dfb3d5f4356602fc0eed8775cbe9
Roman-Ahmad-16 <websitedesigner725@gmail.com>
2025-07-11 19:58:58 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CrudController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/index' , [CrudController :: class , 'index'])->name('crud.index');
-// Route::get('/view' , [CrudController :: class , 'view'])->name('crud.add');
-// Route::post('/store' , [CrudController :: class , 'store'])->name('crud.store');
-// Route::get('/edit' , [CrudController :: class , 'edit'])->name('crud.edit');
-// Route::post('/update' , [CrudController :: class , 'update'])->name('crud.update');
+Route::get('/index' , [CrudController :: class , 'index'])->name('crud.index');
+Route::get('/view' , [CrudController :: class , 'view'])->name('crud.add');
+Route::post('/store' , [CrudController :: class , 'store'])->name('crud.store');
+Route::get('/edit' , [CrudController :: class , 'edit'])->name('crud.edit');
+Route::post('/update' , [CrudController :: class , 'update'])->name('crud.update');
+
+
 
 
 
@@ -22,6 +25,10 @@ Route::get('/addd' , [AboutController :: class , 'addd'])->name('crud.addd');
 Route::post('/storee' , [AboutController :: class , 'storee'])->name('crud.storee');
 Route::get('/editt' , [AboutController :: class , 'editt'])->name('crud.editt');
 Route::post('/updatee' , [AboutController :: class , 'updatee'])->name('crud.updatee');
+
+
+
+
 
 
 Route::get('/books', [BookController::class, 'index'])->name('book.index');
